b96a223f745104eca41944eb1d09e772513655cb
Chris Fawsitt <christopher.fawsitt@cliftoninsight.co.uk>
2022-12-16 08:37:57 +0000

    PAC added with creds

--- test_script.R
+++ test_script.R
@@ -1,3 +1,14 @@
 library(bcea)
 
 ### Description added
+
+# Personal access token - called test token (expires 23 dec)
+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
+
+install.packages("gitcreds")
+library(gitcreds)
+
+gitcreds_set()
+
+library(usethis)
+use_github()
